d9ce8f94862a2f9d40ef27a3822d54b9fb94e4b3
Your Name~farah-jannat <farahjannatmithila@gmail.com>
2022-09-19 16:15:10 +0600

    path to home

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Caterogy from "./components/Caterogy";
 import Pages from "./pages/Pages";
 import{BrowserRouter}from 'react-router-dom';
 import Search from "./components/Search";
+import styled from "styled-components";
+// import {GiknifeFork}from 'react-icons/gi'
+import {Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav>
+        {/* <GiknifeFork/> */}
+        <Logo to ={"/"} >logo </Logo>
+      </Nav>
       <Search />
       <Caterogy/>
       <Pages /> 
@@ -17,5 +24,21 @@ function App() {
    </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration:none;
+  font-size:1.3rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+`
+const Nav = styled.div`
+ padding:4rem 0rem;
+//  display:flex;
+//  justify-content:center;
+//  align-items:center;
+//  svg{
+//   font-size:2rem;
+//  }
+`
 
 export default App;
