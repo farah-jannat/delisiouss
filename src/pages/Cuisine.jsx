885a0084d14a2a9deef0ff808db6462d14bf3bcd
Your Name~farah-jannat <farahjannatmithila@gmail.com>
2022-09-19 15:48:46 +0600

    fix path cuisine

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -28,8 +28,11 @@ useEffect(() =>{
       {Cuisine.map((item) =>{
         return(
           <Card key={item.id}>
+            <Link to ={"/recipe/" +item.id}>
             <img src={item.image} alt="" />
             <h4>{item.title}</h4>
+</Link>
+            
           </Card>
         ) 
       })}
